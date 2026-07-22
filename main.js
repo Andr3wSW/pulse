3087b977cec0b69ebca17c7e649a36623908a49a
Andr3w <scottwebster.andrew@gmail.com>
2026-07-22 13:58:36 -0700

    add more styling and lucide icons

--- main.js
+++ main.js
@@ -48,3 +48,19 @@ revealElements.forEach(element=>{
     observer.observe(element);
 
 });
+
+window.addEventListener("scroll",()=>{
+
+    const scrollTop = window.scrollY;
+
+    const docHeight =
+        document.documentElement.scrollHeight
+        - window.innerHeight;
+
+    const percent =
+        (scrollTop/docHeight)*100;
+
+    document.getElementById("scrollProgress")
+        .style.width = percent + "%";
+
+});
