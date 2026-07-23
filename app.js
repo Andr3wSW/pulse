4cdba68bc8889874326691ca0b269c2bd004a435
Andr3w <scottwebster.andrew@gmail.com>
2026-07-23 00:01:42 -0700

    add working navigation

--- app.js
+++ app.js
@@ -71,3 +71,77 @@ data.firstName
 
 
 });
+
+// ==========================
+// App Navigation
+// ==========================
+
+const buttons =
+document.querySelectorAll(".app-option");
+
+
+const pages = {
+
+    global:
+    document.getElementById("global-page"),
+
+    friends:
+    document.getElementById("friends-page"),
+
+    messages:
+    document.getElementById("messages-page")
+
+};
+
+
+
+buttons.forEach(button=>{
+
+
+    button.addEventListener("click",()=>{
+
+
+        const selected =
+        button.dataset.page;
+
+
+
+        // remove active
+
+        buttons.forEach(btn=>{
+
+            btn.classList.remove("active");
+
+        });
+
+
+
+        button.classList.add("active");
+
+
+
+        // hide everything
+
+        Object.values(pages).forEach(page=>{
+
+            if(page)
+            page.classList.add("hidden-page");
+
+        });
+
+
+
+        // show selected
+
+        if(pages[selected]){
+
+            pages[selected]
+            .classList.remove("hidden-page");
+
+        }
+
+
+    });
+
+
+});
