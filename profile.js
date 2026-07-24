b6ce6d04004db0074bc8765297f6e1cdab04ba1a
Andr3w <scottwebster.andrew@gmail.com>
2026-07-24 15:35:07 -0700

    fix bugs

--- profile.js
+++ profile.js
@@ -1,17 +1,16 @@
 import { db, auth } from "./firebase.js";
 
-
 import {
-
 doc,
-getDoc
-
+getDoc,
+addDoc,
+collection,
+query,
+where,
+getDocs
 }
-
 from
-
 "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
 
 
 
@@ -21,7 +20,6 @@ document.getElementById("profileModal");
 
 const close =
 document.getElementById("closeProfile");
-
 
 
 const name =
@@ -45,21 +43,19 @@ document.getElementById("profileAction");
 
 
 
+let viewedUID;
+
 
 
 export async function openProfile(uid){
 
 
+viewedUID = uid;
+
+
 const snap =
 await getDoc(
-
-doc(
-db,
-"users",
-uid
-
-)
-
+doc(db,"users",uid)
 );
 
 
@@ -71,7 +67,6 @@ return;
 
 const user =
 snap.data();
-
 
 
 
@@ -92,6 +87,7 @@ user.bio || "No bio yet";
 
 if(user.profilePicture){
 
+
 picture.innerHTML =
 `
 <img src="${user.profilePicture}">
@@ -101,10 +97,16 @@ picture.innerHTML =
 
 else{
 
+
 picture.textContent =
 user.firstName[0];
 
 }
+
+
+
+
+await updateButton();
 
 
 
@@ -118,11 +120,206 @@ modal.classList.remove(
 
 
 
+
+
+
+async function updateButton(){
+
+
+const current =
+auth.currentUser;
+
+
+
+if(!current)
+return;
+
+
+
+// Viewing yourself
+
+if(current.uid === viewedUID){
+
+
+action.textContent =
+"Your Profile";
+
+
+action.disabled = true;
+
+
+return;
+
+}
+
+
+
+
+// Check friendship
+
+
+const friendCheck =
+await getDocs(
+
+query(
+
+collection(db,"friends"),
+
+where(
+"user",
+"==",
+current.uid
+),
+
+where(
+"friend",
+"==",
+viewedUID
+)
+
+)
+
+);
+
+
+
+if(!friendCheck.empty){
+
+
+action.textContent =
+"Friends";
+
+
+action.disabled = true;
+
+
+return;
+
+}
+
+
+
+
+
+// Check sent request
+
+
+const requestCheck =
+await getDocs(
+
+query(
+
+collection(db,"friendRequests"),
+
+where(
+"from",
+"==",
+current.uid
+),
+
+where(
+"to",
+"==",
+viewedUID
+)
+
+)
+
+);
+
+
+
+if(!requestCheck.empty){
+
+
+action.textContent =
+"Request Sent";
+
+
+action.disabled = true;
+
+
+return;
+
+}
+
+
+
+
+
+action.textContent =
+"Add Friend";
+
+
+action.disabled = false;
+
+
+}
+
+
+
+
+
+
+
+
+action.onclick =
+async()=>{
+
+
+const current =
+auth.currentUser;
+
+
+if(!current || !viewedUID)
+return;
+
+
+
+await addDoc(
+
+collection(db,"friendRequests"),
+
+{
+
+from:
+current.uid,
+
+
+to:
+viewedUID,
+
+
+createdAt:
+Date.now()
+
+}
+
+);
+
+
+
+action.textContent =
+"Request Sent";
+
+
+action.disabled = true;
+
+
+};
+
+
+
+
+
+
 close.onclick =
 ()=>{
+
 
 modal.classList.add(
 "hidden-page"
 );
+
 
 };
